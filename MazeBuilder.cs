ef0f400615b9f46b8567e12752b81e16562f3b8e
Durim Miziraj <durim.miziraj@gmail.com>
2024-07-22 01:37:39 +0200

    Cleaned the code

--- MazeBuilder.cs
+++ MazeBuilder.cs
@@ -1,18 +1,12 @@
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Globalization;
-using static MazeMaker.TileSettings;
 namespace MazeMaker;
 public class MazeBuilder
 {
     private readonly UInt16 _rowMax;
     private readonly UInt16 _colMax;
     private readonly UInt16 _borderWidth;
-    private readonly ITile[] _tiles = new ITile[0];
-    private readonly (UInt16 width, UInt16 height) _canvasSize;
     private readonly (UInt16 width, UInt16 height) _tileSize;
-    private static readonly Random random = new Random();
-
 
     public MazeBuilder(
         (UInt16 width, UInt16 height) tileSize,
@@ -25,8 +19,9 @@ public class MazeBuilder
         _borderWidth = borderWidth;
         _rowMax = rows;
         _colMax = columns;
-        _canvasSize = CalculateCanvasSize();
-        bmp = new Bitmap(_canvasSize.width, _canvasSize.height);
+
+        (UInt16 width, UInt16 height) canvasSize = CalculateCanvasSize();
+        TileSettings.bmp = new Bitmap(canvasSize.width, canvasSize.height);
 
         TileSettings.Initialize(
             borderColor: Color.Black,
@@ -35,44 +30,52 @@ public class MazeBuilder
             borderWidth: _borderWidth
         );
 
+        BuildMaze();
+        SaveMaze();
+    }
+
+    private void BuildMaze()
+    {
         for (int col = 0; col <= _colMax; col++)
         {
             for (int row = 0; row < _rowMax; row++)
             {
                 new WestSouthTile(
                     fillColor: ColorTranslator.FromHtml("#444444"),
-                    xOrigin: (UInt16)((_tileSize.width - BorderWidth)*col),
-                    yOrigin: (UInt16)((_tileSize.height - BorderWidth)*row),
+                    xOrigin: (UInt16)((_tileSize.width - _borderWidth)*col),
+                    yOrigin: (UInt16)((_tileSize.height - _borderWidth)*row),
                     walls: GetRandomEnumValue<OpenWall>()
                 );
             }
         }
+    }
 
+    private void SaveMaze()
+    {
         DateTime date = DateTime.Now;
-        // Get the parent directory of the current directory
+
         string parentDirectory = Directory.GetParent(Directory.GetCurrentDirectory())!.FullName;
-        string directoryPath = Path.Combine(parentDirectory, "Mazes", date.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
-        string filePath = Path.Combine(directoryPath, $"maze_{date.ToString("HH_mm_ss_fff", CultureInfo.InvariantCulture)}.png");
+        string directoryPath = Path.Combine(parentDirectory, "Mazes", date.ToString("yyyy-MM-dd"));
+        string filePath = Path.Combine(directoryPath, $"maze_{date.ToString("HH_mm_ss_fff")}.png");
 
         try
         {
-            // Ensure the directory exists
             Directory.CreateDirectory(directoryPath);
-
-            // Save the bitmap with the generated file path
-            bmp.Save(filePath, ImageFormat.Png);
-
-            Console.WriteLine($"Image saved to: {filePath}");
+            TileSettings.bmp!.Save(filePath, ImageFormat.Png);
+            Console.WriteLine($"Maze saved to: {filePath}");
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Failed to save the image. Exception: {ex.Message}");
+            Console.WriteLine($"Failed to save the maze. Exception: {ex.Message}");
         }
     }
+
     public static T GetRandomEnumValue<T>() where T : Enum
     {
+        Random random = new Random();
         Array values = Enum.GetValues(typeof(T));
-            T randomValue = (T)values.GetValue(random.Next(values.Length))!;
+        T randomValue = (T)values.GetValue(random.Next(values.Length))!;
+
         return randomValue;
     }
 
@@ -80,16 +83,20 @@ public class MazeBuilder
     {
         Random random = new Random();
         int colorValue = random.Next(0x1000000); 
+
         return $"#{colorValue:X6}";
     }
     
     private (UInt16, UInt16) CalculateCanvasSize()
     {
+        UInt16 canvasWidth = (UInt16)((_tileSize.width - _borderWidth)*_colMax);
+        UInt16 canvasHeight = (UInt16)((_tileSize.height - _borderWidth)*_rowMax);
 
-        Console.WriteLine($"\n\nCanvas size: {(UInt16)((_tileSize.width - _borderWidth)*_colMax)}x{(UInt16)((_tileSize.height - BorderWidth)*_rowMax)}\n\n");
+        Console.WriteLine($"\n\nCanvas size: {canvasWidth}x{canvasHeight}\n\n");
+        
         return (
-            (UInt16)((_tileSize.width - _borderWidth)*_colMax),
-            (UInt16)((_tileSize.height - _borderWidth)*_rowMax)
+            canvasWidth,
+            canvasHeight
         );
     }
 }
